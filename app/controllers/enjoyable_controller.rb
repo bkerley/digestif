fe81e4b32861eddb01a834b9970eb3f62fe859eb
Bryce Kerley <bryce@worldmedia.net>
2008-09-22 17:09:26 -0400

    use helper method

--- app/controllers/enjoyable_controller.rb
+++ app/controllers/enjoyable_controller.rb
@@ -1,8 +1,8 @@
 class EnjoyableController < ApplicationController
   def diversion
     authenticate_or_request_with_http_digest("Diversions") do |username, verifier|
-      # dhh/secret
-      username == "dhh" && verifier['7912bf14316c02bdd0eaf32229fdd30d']
+      # username:realm:password
+      username == "dhh" && verifier[digest_credentials('dhh', 'secret', 'Diversions')]
     end
   end
 
