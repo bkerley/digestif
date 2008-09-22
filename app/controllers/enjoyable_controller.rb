afda4e02148723bd2c6e0f4948c9104c5299099b
Bryce Kerley <bryce@worldmedia.net>
2008-09-22 16:53:21 -0400

    authenticate with dhh/secret

--- app/controllers/enjoyable_controller.rb
+++ app/controllers/enjoyable_controller.rb
@@ -1,5 +1,9 @@
 class EnjoyableController < ApplicationController
   def diversion
+    authenticate_or_request_with_http_digest("Diversions") do |username, verifier|
+      # dhh/secret
+      username == "dhh" && verifier['7912bf14316c02bdd0eaf32229fdd30d']
+    end
   end
 
 end
